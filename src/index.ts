181ce7a1592b5c2f55eeba678a66d2d7469a29a8
JayMartMedia <Jay@JayMartMedia.com>
2021-02-02 13:21:07 -0500

    Added interface

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@ import express from 'express';
 
 const app = express();
 
-const add = (a: number, b?: number): number => {
-    if(b){
-        return a + b;
-    }else{
-        return a;
-    }
+interface Params {
+    a: number
+    b: number
+}
+
+const add = (x: Params) => {
+    return x.a + x.b;
 }
 
 app.get('/', (req) => {
-    add(1);
+    add({a:1,b:2});
 })
 
 app.listen(3001, () => {
